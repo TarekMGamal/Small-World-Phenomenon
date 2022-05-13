c1e727c7d79212107f2f20e05cbbfc2076a0b690
lenovo <tarek20191700323@cis.asu.edu.eg>
2022-05-13 13:53:52 +0200

    Added BFS Updated graph structure

---
+++ Algorithms.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Algorithms
+{
+    internal static class Algorithms
+    {
+        static int VisIndex;
+        static Dictionary<string, List<string>> AdjList;
+        static Dictionary<string, bool> IsActor;
+        static Dictionary<string, int> Visited;
+        static Dictionary<string, int> Distance;
+        static Dictionary<string, string> Prev;
+
+        public static void Prepare(Dictionary<string, List<string>> adjList,
+                                   Dictionary<string, bool> isActor,
+                                   Dictionary<string, int> visited,
+                                   Dictionary<string, int> dis,
+                                   Dictionary<string, string> pre)
+        {
+            VisIndex = 0;
+            AdjList = adjList;
+            IsActor = isActor;
+            Distance = dis;
+            Prev = pre;
+            Visited = visited;
+        }
+
+        private static int Bfs(string node, string dest)
+        {
+            Visited[node] = VisIndex;
+            Distance[node] = 0;
+
+            Queue<string> queue = new();
+            queue.Enqueue(node);
+            
+            while (queue.Count > 0)
+            {
+                node = queue.Dequeue();
+                  
+                if (node == dest)
+                {
+                    return Distance[dest];
+                }
+
+                foreach(var child in AdjList[node])
+                {
+                    if (Visited[child] == VisIndex) continue;
+
+                    Visited[child] = VisIndex;
+
+                    Distance[child] = Distance[node];
+                    if (!IsActor[node]) Distance[child]++;
+
+                    Prev[child] = node;
+
+                    queue.Enqueue(child);
+                }
+            }
+
+            return 0;
+        }
+
+        private static List<string> GetPath(string actor1, string actor2)
+        {
+            List<string> path = new();
+            string node = actor2;
+
+            do
+            {
+                if (!IsActor[node])
+                {
+                    path.Add(node);
+                }
+                node = Prev[node];
+            }
+            while (node != actor1);
+            
+            return path;
+        }
+
+        private static int GetStrength()
+        {
+            // Todo
+            return 0;
+        }
+
+        public static (int, int, List<string>) Query(string actor1, string actor2)
+        {
+            VisIndex++;
+            int degree = Bfs(actor1, actor2);
+            int strength = GetStrength();
+            List<string> path = GetPath(actor1, actor2);
+
+            return (degree, strength, path);
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -9,14 +9,36 @@ namespace Program
 {
     public class Program
     {
+        static Dictionary<string, List<string>> AdjList;
+        static Dictionary<string, bool> isActor;
+        static Dictionary<string, int> vis;
+        static Dictionary<string, int> dis;
+        static Dictionary<string, string> pre;
+
         static void Main(string[] args)
         {
+            AdjList = new();
+            isActor = new();
+            vis = new();
+            dis = new();
+            pre = new();
+
             string fileName = "../../../Testcases/Sample/movies1.txt";
-
-            string tempMovieData = ReadMoviesFile(fileName);
-            Dictionary<string, List<Edge.Edge>> AdjList = GetAdjList(tempMovieData);
-
             
+            string tempMovieData = ReadMoviesFile(fileName);
+            Prepare(tempMovieData);
+
+            Algorithms.Algorithms.Prepare(AdjList, isActor, vis, dis, pre);
+
+            (int, int, List<string>) x = Algorithms.Algorithms.Query("A", "B");
+            Console.Write(x.Item1 + " ");
+            Console.Write(x.Item2 + " ");
+            Console.Write(x.Item3.Count + " ");
+            Console.WriteLine();
+            for (int i = 0; i < x.Item3.Count; i++)
+            {
+                Console.WriteLine(x.Item3[i]);
+            }
         }
 
         static string ReadMoviesFile(string fileName)
@@ -33,44 +55,52 @@ namespace Program
         }
 
         
-        static Dictionary<string, List<Edge.Edge>> GetAdjList(string moviesData)
+        static void Prepare(string moviesData)
         {
             string[] movies = moviesData.Split("\n");
-            Dictionary<string, List<Edge.Edge>> AdjList = new();
-
-
+            
+            
             for (int i = 0; i < movies.Length; i++)
             {
                 string[] movieData = movies[i].Split('/');
 
-                string movieName = movieData[0];
+                string u = movieData[0];
+
+                if (!isActor.ContainsKey(u)) isActor.Add(u, false);
+                else isActor[u] = false;
+
+                if (!vis.ContainsKey(u)) vis.Add(u, 0);
+                else vis[u] = 0;
+
+                if (!dis.ContainsKey(u)) dis.Add(u, 0);
+                else dis[u] = 0;
+
+                if (!pre.ContainsKey(u)) pre.Add(u, "");
+                else pre[u] = "";
 
                 for (int j = 1; j < movieData.Length; j++)
                 {
-                    for (int k = j+1; k < movieData.Length; k++)
-                    {
-                        string u = movieData[j];
-                        string v = movieData[k];
+                    string v = movieData[j];
 
-                        Edge.Edge edge1 = new(u, v, movieName);
-                        Edge.Edge edge2 = new(v, u, movieName);
+                    if (!isActor.ContainsKey(v)) isActor.Add(v, true);
+                    else isActor[v] = true;
 
-                        if (!AdjList.ContainsKey(u))
-                        {
-                            AdjList.Add(u, new List<Edge.Edge>());
-                        }
-                        if (!AdjList.ContainsKey(v))
-                        {
-                            AdjList.Add(v, new List<Edge.Edge>());
-                        }
+                    if (!vis.ContainsKey(v)) vis.Add(v, 0);
+                    else vis[v] = 0;
 
-                        AdjList[u].Add(edge1);
-                        AdjList[v].Add(edge2);
-                    }
+                    if (!dis.ContainsKey(v)) dis.Add(v, 0);
+                    else dis[v] = 0;
+
+                    if (!pre.ContainsKey(v)) pre.Add(v, "");
+                    else pre[v] = "";
+
+                    if (!AdjList.ContainsKey(u)) AdjList.Add(u, new List<string>());
+                    if (!AdjList.ContainsKey(v)) AdjList.Add(v, new List<string>());
+
+                    AdjList[u].Add(v);
+                    AdjList[v].Add(u);
                 }
             }
-
-            return AdjList;
         }
     }
 }
